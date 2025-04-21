6248d8ee2e722f6bea0cd080ca3c7d3ab3bca71a
Abhishek D <abhidhaware1234@gmail.com>
2025-04-21 19:25:34 +0530

    added some hooks

---
+++ WebDevMastery/hooks-in-react/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// impoting the UseState 
+import UseState from './Hooks/useState/UseState'
+import UseEffect from './Hooks/useEffect/UseEffect'
+
+import BgChanger from './Hooks/useLayoutEffect/BgChanger'
+
+const App = () => {
+  return (
+    <>
+      {/* <UseState /> */}
+      
+      {/* <UseEffect /> */}
+
+      < BgChanger />
+    </>
+  )
+}
+
+export default App
