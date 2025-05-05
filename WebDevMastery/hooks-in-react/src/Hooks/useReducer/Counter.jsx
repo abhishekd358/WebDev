5b2fc73419d08e1653763804494394774c77307b
Abhishek D <abhidhaware1234@gmail.com>
2025-05-05 14:05:03 +0530

    added the useMemo Hook

--- WebDevMastery/hooks-in-react/src/Hooks/useReducer/Counter.jsx
+++ WebDevMastery/hooks-in-react/src/Hooks/useReducer/Counter.jsx
@@ -1,16 +1,16 @@
-import React, { useReducer } from 'react'
+import React,{useState} from 'react'
 
 
-// reducer
-
-const CounterReducer = (state, action) =>{ //here we passing one action on that basis we swith to cases
-  switch(action.type){
+const CounterReducer = (state, action) =>{
+  switch (action.type) {
     case 'INCREMENT':
-      return { count: state.count + 1 }
+      return ({count : state.count+1});
+
     case 'DECREMENT':
-      return {count : state.count - 1}
+      return ({count : state.count-1});
+  
     default:
-      return state;
+      return({count: state.count})
   }
 
 }
@@ -18,19 +18,10 @@ const CounterReducer = (state, action) =>{ //here we passing one action on that 
 
 
 const Counter = () => {
-  const [state, dispatch] = useReducer(CounterReducer, {count : 0})
-
+  const [count, setCount] = useState(0)
   return (
-    <div>
-      <h1>counter value : {state.count}</h1>
-      <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
-      <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
-
-    </div>
+    <div>Counter</div>
   )
 }
 
-
-
-// here we exporting the COunter method not Counter Reducer
-export default Counter;
+export default Counter
