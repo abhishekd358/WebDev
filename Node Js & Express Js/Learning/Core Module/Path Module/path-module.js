9060a012f2e665e42b8da1f091155a8947f41fe5
Abhishek D <abhidhaware1234@gmail.com>
2025-05-08 21:38:26 +0530

    added few methods

--- Node Js & Express Js/Learning/Core Module/Path Module/path-module.js
+++ Node Js & Express Js/Learning/Core Module/Path Module/path-module.js
@@ -7,9 +7,32 @@ import path from 'path';
 // returns absolute path of file
 
 const abs_path = path.resolve('path-module.js')
-console.log('▶▶▶',abs_path)
+console.log('1) ▶▶▶',abs_path)
 
 // 2. ============================= extname ==============================
 // return file extension
 const file_ext_name = path.extname('../Path Module/MyNewFile.py')
-console.log('🔶🔶🔶',file_ext_name)
+console.log('2) 🔶🔶🔶',file_ext_name)
+
+// 3. ============================= basename ==============================
+// extract and return only the file name
+const file_name = path.basename('./fs Module/myBio.txt');
+console.log('3)', file_name);
+
+// 4. ============================= dirname ==============================
+// extract and return only the directory name 
+const directory_name = path.dirname('./fs Module/myBio.txt');
+console.log('4)', directory_name);
+
+
+// 5. ============================= join(...path) ==============================
+// joins multiple paths and return a string of  directory from it
+const join_path = path.join('public', 'main', 'src', 'component');
+console.log('5)', join_path);
+
+
+// 6. ============================= parse(...path) ==============================
+// return inforamtion about file directory like root, dir, base, ext, name
+const parse_data = path.parse('./fs Module/myBio.txt');
+console.log('6)', parse_data);
+
