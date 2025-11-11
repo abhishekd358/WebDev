a0d5ffa1b3f4c478f9ff245445d1279e19d555e2
Abhishek D <abhidhaware1234@gmail.com>
2025-11-11 15:24:50 +0530

    Fun of Node: added the Hybrid-custom-package creation

---
+++ Fundamental of Node/3.Npm_modules/hybrid_package_msg.js
@@ -0,0 +1,9 @@
+// 1______________________________ modular js import
+import {msg} from 'hybrid-package'
+console.log(msg);
+
+
+
+// 2______________________________ common js import
+const {msg} = require('hybrid-package')
+console.log(msg);
--- Fundamental of Node/3.Npm_modules/mjs_package_msg.js
+++ Fundamental of Node/3.Npm_modules/mjs_package_msg.js
@@ -1,3 +1,5 @@
 import {msg} from 'es6-msg-package'
 
 console.log(msg)
+
+
