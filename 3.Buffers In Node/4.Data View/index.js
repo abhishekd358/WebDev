285fb528b9a85fb32f9f3ed52a3a93222403c505
Abhishek D <abhidhaware1234@gmail.com>
2025-11-15 15:57:15 +0530

    3)Buffer: DataView in ArrayBuffer

---
+++ 3.Buffers In Node/4.Data View/index.js
@@ -0,0 +1,20 @@
+// creating ArrayBuffer which is empty
+const buff = new ArrayBuffer(4)
+// console.log(buff)
+
+// now if we wnat to read and write data in buffer on each index we need to create DataView
+// DataView
+const view = new DataView(buff)
+// console.log(view);
+
+// now we want to set data
+view.setInt8(2, -10)  // setting negative value
+view.setUint8(1, -10)  // this can't set neagtive value beacuse it is itself Unsigned
+
+console.log(view);
+
+
+console.log(view.getInt8(2)) // this return our neagtive value
+console.log(view.getUint8(1))  // it not negative value that we set it alwayas Unsigned
+
+
