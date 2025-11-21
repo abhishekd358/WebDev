8eb4587893bde0e16cbb0af634cb8ca0622e2468
Abhishek D <abhidhaware1234@gmail.com>
2025-11-21 13:03:55 +0530

    Coders Gyan: added different ways to do pipleine and error handlig on streams

--- 4.Streams/coder's gyan/server.js
+++ 4.Streams/coder's gyan/server.js
@@ -1,8 +1,9 @@
 // first we create a server http
 
 import { createServer } from "node:http";
-import { createReadStream, readFileSync } from "node:fs";
+import { createReadStream, createWriteStream, readFileSync } from "node:fs";
 import {readFile} from 'fs/promises'
+import { pipeline, Transform } from "node:stream";
 
 const server = createServer(async (req, res) => {
     // home route
@@ -45,6 +46,71 @@ const server = createServer(async (req, res) => {
   }
 
 
+
+
+
+// ========================================= FILEE PROCESSING / TRANSFORM
+  // GOODWAY: fs sync
+  if (req.url == "/goodway") {
+    // ----------------------------------1: Bad way to send file to client from server
+    const file = createReadStream("datafile.txt");
+
+    // we write on file data on new output.txt on receiver end
+    const outputFile = createWriteStream('output.txt')
+
+// METHOD 1:-------------------------------------------------------------------
+    // // so as we recive cunk of data from readblestream then we push to output.txt
+    // file.on('data', (chunk)=>{
+    //     // PROCESS KARENGE / TRANSFORM KARNEGE in between each chunk when it flowing to RECEIVER end.
+    //     const uppercaseModification = chunk.toString().toUpperCase()
+    //     // jaise hi process kiya hua data aajyega vaise hi hum kya write kar denge
+    //     outputFile.write(uppercaseModification)
+    // })
+
+//but above .on() is not good way to TRANSFORM / PROCESS / MODIFY data when our data in flow 
+
+
+// METHOD 2:---------------------------------------------------------------------
+//  for that transform() use
+
+  const uppercaseModificationStream = new Transform({
+    transform(chunk, encoding, callback){
+
+      // here we transform the flowing data chunk before receive to end user
+      const uppercaseModification = chunk.toString().toUpperCase()
+      // now we process 
+      // now we have to send process data to next pipe
+      callback(null,uppercaseModification )   // has parameter error, data
+    } 
+  })
+
+
+// wehen we do transfrom  we have to add in pipe
+// file(source) ----> trasfromStream(process) -----> outputFile(receiver)
+
+
+// file.pipe(uppercaseModificationStream).pipe(outputFile)
+
+// file.pipe(uppercaseModificationStream).on('error',()=>console.log(error)).pipe(outputFile).on('error',()=>console.log(error))
+// -------------------------- limitiation on this above is that we not able to handle the error if occur when during flow of data and we processing  
+
+// but this above error handling is not good way ---------------------
+
+
+
+
+
+
+// METHOD 3 ______________________Instead _pipe()____we_use_pipeline()_______________________________________
+
+// thatswhy pipeline used
+pipeline(file, uppercaseModificationStream, outputFile,
+  (error)=>console.log(error)
+)
+// source ----> transform stram ----> receiver
+
+
+}
 
 
 });
