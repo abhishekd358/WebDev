162eef0b4793f051b0d976cb211dd2ed9a0bdcd2
Abhishek D <abhidhaware1234@gmail.com>
2025-12-10 17:24:02 +0530

    7) Express : added 2 ways of multer file store

--- 7.Express/b)Multer/app.js
+++ 7.Express/b)Multer/app.js
@@ -4,7 +4,7 @@ import multer from 'multer'
 const app = express()
 
 // -------------------------------------------2) create a folder were multer store 
-const fileUpload = multer({ dest: 'uploads/' })
+// const fileUpload = multer({ dest: 'uploads/' })
 
 
 
@@ -18,12 +18,30 @@ app.get('/', (req, res)=>{
 
 // --------------------------------------------3) now here on fileUpload we have .single('attribute_name_passing_through_frontend') method for 1 file upload 
 // ---------------------- file upload
-app.post('/upload',fileUpload.single('file'), (req, res)=>{
-    console.log(req.file)
-    res.write("file uploaded")
+// app.post('/upload',fileUpload.single('file'), (req, res)=>{
+//     console.log(req.file)
+//     res.send("file uploaded")
+// })
+
+
+// +++++++++++++++++++++++++++++++++++ DISK STORAGE METHOD ++++++++++++++++++++++++++++++++++++++++++++
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'Storage')
+  },
+  filename: function (req, file, cb) {
+    // Storing file with original name
+    cb(null, file.originalname);  
+  }
 })
 
+const fileStorage = multer({ storage })
 
-
+// route
+app.post('/disk', fileStorage.fields([{name:'secondMethod'}]),(req, res)=>{
+    const fileData = req.files
+    console.log(fileData)
+    res.send("2)file uploaded")
+})
 
 app.listen(8000, ()=>console.log("Server Running on Port 8000"))
