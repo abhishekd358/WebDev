c388e090d9db8aeee1fed1961dd30a9e6392a8af
Abhishek D <abhidhaware1234@gmail.com>
2025-12-27 23:00:43 +0530

    10) Auth : added Stateful vs Serverless and Hash and encyrption

---
+++ 8.MongoDB/mongodb-practise/Procodrr-finding-the-insight-of-cookie-expire.js
@@ -0,0 +1,12 @@
+import {ObjectId} from 'mongodb'
+
+
+const myId = new ObjectId("69019b13a565da44d9d6748c")
+// console.log(myId._bsontype)
+console.log(myId.getTimestamp())
+console.log(parseInt("507f1f77b", 16))
+console.log(1769424096- 1766832096)
+
+console.log((2592000/86400))
+console.log((30))
+
