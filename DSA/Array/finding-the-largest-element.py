177f2ce07410e6c327230d8658d057cd9d809486
Abhishek D <abhidhaware1234@gmail.com>
2026-02-17 17:25:32 +0530

    addded : finding-the-largest-element

---
+++ DSA/Array/finding-the-largest-element.py
@@ -0,0 +1,24 @@
+num  =[1 ,8, 7, 56, 90]
+def largestElement(num):
+    largest = 0
+
+    '''================= Approach 1'''
+    for i in range(len(num)-1):
+        if(num[i]> num[i+1]):
+            largest = num[i]
+        else:
+            largest = num[i+1]            
+            
+    print(largest)
+
+    '''================= Approach 2'''
+    for i in range(len(num)):
+        if(num[i]> largest):
+            largest = num[i]
+    print(largest)
+
+
+
+
+largestElement(num)
+            
