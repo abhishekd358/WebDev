e1d4fe13209b9109d538f20488d45f63c1ea977c
Abhishek D <89733722+abhishekd358@users.noreply.github.com>
2026-02-21 10:40:03 +0530

    12) REDIS : parse and stringfy the client and server data

--- 12.Redis/Connecting redis/redis-connection.mjs
+++ 12.Redis/Connecting redis/redis-connection.mjs
@@ -4,28 +4,39 @@ import {createClient} from 'redis'
 
 const redisClient = createClient({url:"redis://localhost:6379"})
 
+const employee = {
+    'dept' : 'electrical',
+    'salary': 200,
+    'duty-hrs' : 12
+}
+
 //  connect event
-redisClient.on("connect", ()=>console.log('Redis connected successfully on 6379'))
+await redisClient.on("connect", async()=>{console.log('Redis connected successfully on 6379')
+     redisClient.set('city', 'abhi')
+     redisClient.set('employee', JSON.stringify(employee))
+    console.log(JSON.parse(await redisClient.get('employee')))
+})
+
+
 
 // ready event
-redisClient.on('ready',() =>console.log("Redis ready to use"))
-
+// redisClient.on('ready',() =>console.log("Redis ready to use"))
 
 // end event
 redisClient.on("end", () =>console.log("Redis connection closed"))
 
 // error event
-redisClient.on("error", (err) =>console.error("Redis error:", err))
+// redisClient.on("error", (err) =>console.error("Redis error:", err))
 
 
 
-redisClient.connect()
+await redisClient.connect()
 
 
 setTimeout(() => {
     console.log('Disconnecting the redis.....')
     redisClient.quit()
-}, 2000);
+}, 10000);
 
 
 
