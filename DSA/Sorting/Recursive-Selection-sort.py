eac575f523a9f33f85364de20ffe47f486de0a8a
Abhishek D <89733722+abhishekd358@users.noreply.github.com>
2026-03-14 15:22:05 +0530

    added in simple way to recursion using for loop

--- DSA/Sorting/Recursive-Selection-sort.py
+++ DSA/Sorting/Recursive-Selection-sort.py
@@ -5,21 +5,20 @@ def recursiveSsort(arr, i):
     # in simple word : haa i manjhe OUTER LOOP samjh okay
     if(i == len(arr)-1):
         return
-    
-    small = arr[i]
-    indx = i
-    j = i+1
-    while(j<len(arr)):
-        print(j)
-        if(arr[j]< small):
-            small = arr[j]
-            indx =j 
-        j+=1
-    # when we get samll we swap
-    arr[i], arr[indx] = small, arr[i]
-    print(arr)
 
+    min = arr[i]
+    indx  = i
+    for j in range(i+1, len(arr)):
+        if(arr[j] < min):
+            # choota hai to min update kar denge . or uska index record kar lenge
+            min = arr[j]
+            indx = j
+    # minimum mil gaya to swap kar denge
+    arr[i], arr[indx] = min, arr[i] 
+
+    print(arr)
     return recursiveSsort(arr, i+1)
+    # return arr
 
 
 
