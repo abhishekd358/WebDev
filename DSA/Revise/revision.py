07e3aca9ddab63fa6a1a0e403cae5f5eee506687
Abhishek D <89733722+abhishekd358@users.noreply.github.com>
2026-04-16 18:20:11 +0530

    StorageApp : added the file size restriction with chunk monitoring

--- DSA/Revise/revision.py
+++ DSA/Revise/revision.py
@@ -105,62 +105,137 @@
 
 
 
-def mergeSort(arr, low, high):
-    # ======== DIVIDE karenge
-    # Base case
-    if low == high:
-        return 
-    mid = (low+ high) // 2
+# def mergeSort(arr, low, high):
+#     # ======== DIVIDE karenge
+#     # Base case
+#     if low == high:
+#         return 
+#     mid = (low+ high) // 2
 
 
-    # ========= MERGE karenge
-    def merge(arr, low, mid ,high):
-        left = low
-        right = mid+1
+#     # ========= MERGE karenge
+#     def merge(arr, low, mid ,high):
+#         left = low
+#         right = mid+1
 
-        # temp arr  
-        temp = []
+#         # temp arr  
+#         temp = []
 
-        # now we sort the each divide or half of array
-        while left <= mid and right <= high:
-            # check karegne
-            if arr[left] < arr[right]:
-                temp.append(arr[left])
-                left+=1
-            else:
-                # right wala chota hai 
-                temp.append(arr[right])
-                right +=1
+#         # now we sort the each divide or half of array
+#         while left <= mid and right <= high:
+#             # check karegne
+#             if arr[left] < arr[right]:
+#                 temp.append(arr[left])
+#                 left+=1
+#             else:
+#                 # right wala chota hai 
+#                 temp.append(arr[right])
+#                 right +=1
 
-        # ab kuch element kisi bhi ek array mai reamin honge us ko copy kar denge
-        while left<= mid:
-            temp.append(arr[left])
-            left+=1
-        while right<=high:
-            temp.append(arr[right])
-            right+=1
+#         # ab kuch element kisi bhi ek array mai reamin honge us ko copy kar denge
+#         while left<= mid:
+#             temp.append(arr[left])
+#             left+=1
+#         while right<=high:
+#             temp.append(arr[right])
+#             right+=1
 
-        #  now we get complete the temp array sorted 
-        for i in range(low , high+1):
-            arr[i] = temp[i-low]
+#         #  now we get complete the temp array sorted 
+#         for i in range(low , high+1):
+#             arr[i] = temp[i-low]
 
-        return arr
-
-
-# =============== Recurisoion calls
-    # first half
-    mergeSort(arr, low, mid)
-    # second half
-    mergeSort(arr, mid+1, high)
-
-    # both get merge
-    merge(arr, low, mid, high)
-    return arr
+#         return arr
 
 
+# # =============== Recurisoion calls
+#     # first half
+#     mergeSort(arr, low, mid)
+#     # second half
+#     mergeSort(arr, mid+1, high)
 
-
-print(mergeSort([7, 4, 1, 5, 3], low=0, high=4))
+#     # both get merge
+#     merge(arr, low, mid, high)
+#     return arr
 
 
 
+
+# print(mergeSort([7, 4, 1, 5, 3], low=0, high=4))
+
+
+
+
+
+
+# ========================= discout calculate
+# import math
+# amount = int(input())
+
+# if amount < 1000:
+#     print(f'{(amount * 5) / 100:.2f}')
+    
+# elif (1000 <= amount< 5000):
+#     print(f'{(amount * 10) / 100:.2f}')
+
+# else:
+#     print(f'{(amount * 15) / 100:.2f}')
+
+
+# n = int(input())
+# arr = list(map(int, input().split()))
+# maxSum = int(input())
+
+
+# prevSum = 0
+# for i in range(len(arr)):
+#     currSum = 0
+#     for j in range(i, len(arr)):
+#         currSum+= arr[j]
+#         if currSum <= maxSum:
+#             prevSum = max(prevSum , currSum)
+# print(prevSum)
+
+
+# ====================== DP prblem 
+# def countWays(N, R, end):
+
+#     # step 1 (only one element → must be 1)
+#     if end == 1:
+#         same = 1
+#         diff = 0
+#     else:
+#         same = 0
+#         diff = 1   # [1] ends witnot-endh 
+
+#     # build for length 2 → N
+#     for i in range(2, N+1):
+
+#         new_same = diff   # only way to reach 'end'
+
+#         new_diff = same*(R-1) + diff*(R-2)
+
+#         same = new_same
+#         diff = new_diff
+
+#     return same
+
+
+# print(countWays(4,4,3))  # 7
+# print(countWays(3,3,2))  # 2
+
+# ===========================================================
+#  taking input 
+
+N = int(input().strip())
+
+temp = []
+
+for i in range(N):
+    a , b = map(int, input.split())
+    temp.append([a, b])
+
+
+
+# output 
+for a, b in temp:
+    print(a, b)
--- StorageApp/backend/controllers/fileController.js
+++ StorageApp/backend/controllers/fileController.js
@@ -3,6 +3,7 @@ import { rm } from "fs/promises";
 import path from "path";
 import Directory from "../models/directoryModel.js";
 import File from "../models/fileModel.js";
+import {pipeline} from 'node:stream/promises'
 
 export const uploadFile = async (req, res, next) => {
   const parentDirId = req.params.parentDirId || req.user.rootDirId;
@@ -18,8 +19,25 @@ export const uploadFile = async (req, res, next) => {
     }
 
     const filename = req.headers.filename || "untitled";
-    const filesize = req.headers.filesize;
+    const filesize = Number(req.headers.filesize);
+    // console.log(filename, filesize)
     // console.log(typeof filesize);
+
+    //  Blocking the request if the SIze is greater 
+    if(filesize > 50 * 1024 * 1024){
+      // 1. Set the status code
+      res.status(413);
+        
+      // 2. we send a msg
+      console.log('file size big')
+      res.write('File too large. Connection terminated.');
+        
+      // 3. Destroy the connection immediately
+      return res.destroy(); 
+    }
+
+
+
     const extension = path.extname(filename);
 
     const insertedFile = await File.insertOne({
@@ -33,9 +51,45 @@ export const uploadFile = async (req, res, next) => {
     const fileId = insertedFile.id;
 
     const fullFileName = `${fileId}${extension}`;
+    const filePath = `./storage/${fullFileName}`
 
-    const writeStream = createWriteStream(`./storage/${fullFileName}`);
-    req.pipe(writeStream);
+    const writeStream = createWriteStream(filePath);
+    //============ checking the size of file when receiving to sever using chunk . so from postamn or frotend even after manumulation of file size we validate every chuck size . this help less bandwidth use and security 
+    let totalFileSize = 0
+    let isAborted = false
+    req.on('data', async(chunk)=>{
+
+      // if aborted 
+      if(isAborted){
+        return 
+      }
+
+      totalFileSize += chunk.length
+      isAborted = true
+      if (totalFileSize > filesize){
+        // remove the file also
+        writeStream.close()
+        res.status(413);
+        await insertedFile.deleteOne()
+        await rm(filePath)
+        return res.destroy()
+      }
+
+      // ------------------ Backpressure manual handling
+
+      // we write on our stram
+      let canWrite= writeStream.write(chunk)
+      if (!canWrite){
+        //  if not writable we pasue the readble stream
+        req.paused()
+
+        // else we check drain event
+        writeStream.once('drain', ()=>{
+          // drain ho gaya to resume kar denge
+          req.resume()
+        })
+      }
+    })
 
     req.on("end", async () => {
       return res.status(201).json({ message: "File Uploaded" });
