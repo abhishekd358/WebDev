db058b83ea62b31572080400453d34b48d0ab3d3
Abhishek D <89733722+abhishekd358@users.noreply.github.com>
2026-04-16 23:14:45 +0530

    DSA: revise array problem

---
+++ DSA/Array/revise.py
@@ -0,0 +1,22 @@
+# ========================== Largest Element 
+# ========================== Second Largest Element 
+
+# def secondLargest(arr):
+#     maxVal = arr[0]
+#     secondMax = -1
+
+#     for num in arr:
+#         if  secondMax < num > maxVal:
+#             secondMax = maxVal
+#             maxVal= num
+#         elif secondMax < num< maxVal:
+#             secondMax = num
+#     return secondMax
+             
+
+# print(secondLargest([8, 8, 7, 6, 5]))
+
+
+# ============================ 
+
+
