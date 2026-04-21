d14fd00c1a9f5a9bd5ca411c6a8cf42ea79d4088
Abhishek D <89733722+abhishekd358@users.noreply.github.com>
2026-04-21 14:33:55 +0530

    DSA: rearrange array element by sign [added all the 4 approaches]

--- DSA/Array/Medium/4)leetcode-2149-Rearrange Array Elements by Sign.py
+++ DSA/Array/Medium/4)leetcode-2149-Rearrange Array Elements by Sign.py
@@ -1,3 +1,26 @@
+#  ====================== APPROACH : Optimal :  Time Complexity O(n) , Space Complexity O(n)
+def rearrangeArray1(nums):
+    ans = [0] * len(nums)
+
+    pos_indx = 0
+    neg_indx = 1
+
+    # ek loop start karenge 
+    for i in range(len(nums)):
+        if nums[i] > 0: # postitve number hai kya
+            ans[pos_indx] = nums[i] # to place kr denge
+            pos_indx += 2    # 2 ke interval chalega : 0 , 2 ,4, 6
+               
+        else: # Negative Number hai 
+            ans[neg_indx] = nums[i]  
+            neg_indx += 2    # 2 ke interval chalega : 1 , 3 ,5, 7
+    return ans
+
+print("OPTIMAL SOlution:")
+print(rearrangeArray1([3,1,-2,-5,2,-4]))
+
+
+
 #  ==================== APPROACH 1 : Time Complexity O(n) , Space Complexity O(n)
 
 def rearrangeArray1(nums):
@@ -9,14 +32,16 @@ def rearrangeArray1(nums):
         else:
             pos.append(num)
         
-    inc = 0
+    pos_inc = 0
+    neg_inc = 0
     for i in range(len(nums)):
         if i % 2 == 0: # even 
-            nums[i] = pos[inc]
+            nums[i] = pos[pos_inc]
+            pos_inc += 1
                
         if i % 2 != 0:
-            nums[i] = neg[inc]
-            inc+=1
+            nums[i] = neg[neg_inc]
+            neg_inc+=1
     return nums
 
 print("approach 1:")
@@ -47,3 +72,35 @@ def rearrangeArray2(nums):
 
 print("approach 2:")
 print(rearrangeArray2([3,1,-2,-5,2,-4]))
+
+
+
+
+
+
+
+
+#  ============================== BRUTH FORCE : 
+class Solution:
+    def rearrangeArray(self, nums):
+
+        for i in range(len(nums)):
+
+            if i % 2 == 0: # positive
+                if nums[i] < 0:
+                    # next postive ko lenge us se swap krenge
+                    for j in range(i, len(nums)):
+                        if nums[j] > 0:
+                            nums[i], nums[j]  = nums[j], nums[i]
+                            break
+            
+            # odd
+            if i % 2 != 0:
+                if nums[i] > 0:
+                    # next neagtive ko lenge or us se swap karenge
+                    for j in range(i, len(nums)):
+                        if nums[j] < 0:
+                            nums[i], nums[j]  = nums[j], nums[i]
+                            break
+        
+        return nums
