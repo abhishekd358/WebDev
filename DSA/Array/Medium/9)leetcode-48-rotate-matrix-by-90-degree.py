fd5cb719d705f1b187ce994b0517674aac3c8be2
Abhishek D <89733722+abhishekd358@users.noreply.github.com>
2026-05-09 18:50:43 +0530

    DSA: added leetcode 48 rotate the matrix by 90 degree

---
+++ DSA/Array/Medium/9)leetcode-48-rotate-matrix-by-90-degree.py
@@ -0,0 +1,46 @@
+# ====================================== BRUTH FORCE APPROACH 
+# def rotate(matrix):
+
+#     rows = len(matrix)
+#     cols = len(matrix[0])
+    
+#     # create a temp matrix
+    
+#     tempMatrix = [[0 for _ in range(cols)] for _ in range(rows)]
+#     # n*n
+#     # now we loop 
+#     for r in range(rows):
+#         for c in range(cols):
+#             # swap karenge [r][c] ke postion ko and waha pe current element put kardenge
+#             tempMatrix[c][r] = matrix[r][c]
+    
+#     # n*n
+#     for r in range(rows): # complexity : n 
+#         tempMatrix[r].reverse() # complexoty : n
+
+#     return tempMatrix
+
+# ================================================================ OPTIMAL APPRAOCH
+def rotate(matrix):
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+    
+    # create a temp matrix
+    
+    tempMatrix = [[0 for _ in range(cols)] for _ in range(rows)]
+    # n*n
+    # now we loop 
+    for r in range(rows):
+        for c in range(r, cols):
+            # swap karenge [r][c] ke postion ko and waha pe current element put kardenge
+            matrix[r][c],matrix[c][r] =  matrix[c][r], matrix[r][c]
+    
+    # n*n
+    for r in range(rows): # complexity : n 
+        matrix[r].reverse() # complexoty : n
+
+    return matrix
+
+
+print(rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]))
